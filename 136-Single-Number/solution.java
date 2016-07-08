140d2e97e6b0822c8ebb1c955ea951d969b591a4
wz840 <wz840@nyu.edu>
2016-07-09 01:46:20 +0800

    [136. Single Number][Accepted]committed by LeetCode Extension

--- 136-Single-Number/solution.java
+++ 136-Single-Number/solution.java
@@ -1,5 +1,6 @@
 public class Solution {
     public int singleNumber(int[] nums) {
+        /*
         HashMap<Integer, Integer> ht = new HashMap<Integer, Integer>();
         int single = 0;
         for (int x:nums) {
@@ -14,6 +15,20 @@ public class Solution {
                 break;
             }
         }
-        return single;
+        return single;*/
+        
+	    int bit=0;
+	    int result=0;
+	    for(int i=0;i<32;i++){
+	        bit=0;
+		    for(int j=0;j<nums.length;j++){
+		        if(((nums[j]>>i)&1)==1){  //比方说外循环第一次的时候0 那么就不往右平移 然后和1比 看看是不是1
+				    bit++;             //外循环第二次的时候往右移1位然后比第二位。
+			    }
+		    } 
+		    bit=bit%2;
+		    result=result|bit<<i;//每次外循环结束的时候就把bit取完余后的结果存到result里
+	    }
+	    return result;
     }
 }
