f77e40bd1a9e8c4ae71bd1e1bbcd98f5f9e18ebe
wz840 <wz840@nyu.edu>
2016-07-12 18:22:09 +0800

    [203. Remove Linked List Elements][Accepted]committed by LeetCode Extension

--- 203-Remove-Linked-List-Elements/solution.java
+++ 203-Remove-Linked-List-Elements/solution.java
@@ -11,25 +11,17 @@ public class Solution {
         if (head==null) return null;
         else {
             ListNode n = head;
-            if (isOnlyTail(n,head)) {
-                return removeOnlyTail(n,val);
+            if (head.next==null) {
+                if (head.val==val) return removeTail(head,head);
+                else return head;
             } else {
                 while(n.next!=null) {
                     if (n.val==val){
-                       // if (n.next.next!=null) {
-                            n.val = n.next.val;
-                            n.next = n.next.next;
-                        // } else {
-                        //     n.val = n.next.val;
-                        //     n.next = null;
-                        // }
-                    } else {
-                        n = n.next;
-                    }
+                        removeNode(n);
+                    } else n = n.next;
                 }
-
                 if (n.val==val) {
-                    return removeTail(head,n,val);
+                    return removeTail(head,n);
                 }
                 return head;
             }   
@@ -45,27 +37,13 @@ public class Solution {
         return (node.next==null && node==head);
     }
     
-    public boolean isTail(ListNode node) {
-        return (node.next==null);
-    }
     
     public ListNode removeOnlyTail(ListNode tail, int val) {
         if (tail.val!=val) return tail;
         else return null;
     }
     
-    public void lastSecondNode(ListNode n, int val, ListNode head) {
-        if (!isTail(n)){
-            if (n.next.next==null && n.next.val==val) {
-                n.next = null;
-            } else {
-                return;
-            }
-        } else removeNode(n);
-    }
-    
-    
-    public ListNode removeTail(ListNode head, ListNode tail, int val){
+    public ListNode removeTail(ListNode head, ListNode tail){
         ListNode n = head;
         if (n.next!=null){
             while(n.next!=tail){
@@ -74,7 +52,7 @@ public class Solution {
             n.next = null;
             return head;
         } else {
-            return removeOnlyTail(tail,val);
+            return null;
         }
     }
 }
