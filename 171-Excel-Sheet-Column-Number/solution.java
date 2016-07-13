7e121c4e1ba32ce547e52019b50368ba65f40ce8
wz840 <wz840@nyu.edu>
2016-07-14 00:00:39 +0800

    [171. Excel Sheet Column Number][Accepted]committed by LeetCode Extension

---
+++ 171-Excel-Sheet-Column-Number/solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int titleToNumber(String s) {
+        s = s.toUpperCase();
+        if (s.length()==0) return 0;
+        else {
+            return stringToAsc(s);
+        }
+    }
+    
+    private int stringToAsc(String s) {
+        int num = 0;
+        int bit = s.length()-1;
+        for (int i=0; i<s.length(); i++) {
+            num += ((int)s.charAt(i) - 64) * Math.pow(26,bit-i);
+        }
+        return num;
+    }
+}
