38d397c85f8971883ca8c468aaa3265431ceef50
wz840 <wz840@nyu.edu>
2016-07-14 01:04:52 +0800

    [171. Excel Sheet Column Number][Accepted]committed by LeetCode Extension

--- 171-Excel-Sheet-Column-Number/solution.java
+++ 171-Excel-Sheet-Column-Number/solution.java
@@ -11,7 +11,8 @@ public class Solution {
         int num = 0;
         int bit = s.length()-1;
         for (int i=0; i<s.length(); i++) {
-            num += ((int)s.charAt(i) - 64) * Math.pow(26,bit-i);
+            //num += ((int)s.charAt(i) - 64) * Math.pow(26,bit-i);
+            num += (Integer.valueOf(s.charAt(i)+"", 36) - 9) * Math.pow(26,bit-i);
         }
         return num;
     }
