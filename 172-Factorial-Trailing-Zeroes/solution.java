6a4ddca9837fed4a2ad4ff46557cc64a5926b9b8
wz840 <wz840@nyu.edu>
2016-07-20 22:30:09 +0800

    [172. Factorial Trailing Zeroes][Accepted]committed by LeetCode Extension

--- 172-Factorial-Trailing-Zeroes/solution.java
+++ 172-Factorial-Trailing-Zeroes/solution.java
@@ -1,12 +1,19 @@
 public class Solution {
+    int sum = 0;
     public int trailingZeroes(int n) {
-        int k = 0;
+        /*int k = 0;
         int sum = 0;
         if (n%5==0) k = n/5+1;
         else k = (int)(Math.ceil((Math.log(n))/(Math.log(5))-1));
         for (int i=1; i<=k; i++) {
             sum += (int)Math.floor(n/Math.pow(5,i));
         }
-        return sum;
+        return sum;*/
+        int result = (int)Math.floor(n/5);
+        if (result==0) return sum;
+        else {
+            sum += result;
+            return trailingZeroes(result);
+        }
     }
 }
