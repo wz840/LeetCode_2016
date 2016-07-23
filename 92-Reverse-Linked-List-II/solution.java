810342b29b6cdc18611cfc6810a13ed5a9df56da
wz840 <wz840@nyu.edu>
2016-07-23 12:52:45 +0800

    [92. Reverse Linked List II][Accepted]committed by LeetCode Extension

--- 92-Reverse-Linked-List-II/solution.java
+++ 92-Reverse-Linked-List-II/solution.java
@@ -13,45 +13,40 @@ public class Solution {
         ListNode pointer = null;
         ListNode pivot = null;
 
-        if (head==null) return null;
-        else if (head.next==null) {
+        if (head==null) return null;        //empty list
+        else if (head.next==null) {         // only one node in list
             return head;
         }
-        else {
-            if (m==n) return head;
-            else if (m==1) {
+        else {                              // Multiple nodes in list
+            if (m==n) return head;          // reverse nothing
+            else if (m==1) {                // reverse from the start, make a dummy node as pointer
                 pointer = new ListNode(0);
                 pointer.next = head;
-            } else {
+            } else {                        // traverse the list until we meet the starting node
                 while (i<m) {
                     i++;
-                    if (i==m) pointer = head;
+                    if (i==m) pointer = head;       // mark the node before the starting node as pointer
                     head = head.next;
                 }
             }
-            pivot = head;
-            head = head.next;
-            for (int j=i+1; j<=n; j++) {
+            pivot = head;                           // mark the first node for reverse as the pivot
+            head = head.next;                       // pivot never get swapped
+            for (int j=i+1; j<=n; j++) {             
                 System.out.println(head.val);
-                deleteNode(pivot, head);
-                insertAfter(pointer, head);
-                if (m==1) temp = head;
-                head = pivot.next;
+                deleteNode(pivot, head);            // detach the node right next to pivot
+                insertAfter(pointer, head);         // swap it (insert) after the pointer
+                if (m==1) temp = head;              // if m==1 make the return node as the swapped node   
+                head = pivot.next;                  // because it's a moving head of the list to be returned.
             }
             return temp;
         }
     }
     
-    public void deleteNode(ListNode pivot, ListNode node) {
-        /*if (node.next!=null) {
-            pivot.next = node.next;
-        } else {
-            pivot.next = null;
-        }  */
+    public void deleteNode(ListNode pivot, ListNode node) {             // delete the node from the list, but not set to null
         pivot.next = node.next;
     }
     
-    public void insertAfter(ListNode pointer, ListNode node) {
+    public void insertAfter(ListNode pointer, ListNode node) {          // insert the deleted node after the pointer
         node.next = pointer.next;
         pointer.next = node;
     }
