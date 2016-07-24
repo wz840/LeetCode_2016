e7a4b4bac93973a6d6a830171df2ff252823a321
wz840 <wz840@nyu.edu>
2016-07-24 22:28:52 +0800

    [326. Power of Three][Accepted]committed by LeetCode Extension

--- 326-Power-of-Three/solution.java
+++ 326-Power-of-Three/solution.java
@@ -1,6 +1,6 @@
 public class Solution {
     public boolean isPowerOfThree(int n) {
-        double epsilon = 0.00000000001;
+        double epsilon = 0.00000000000001;
         return (Math.log(n) / Math.log(3) + epsilon) % 1 <= 2 * epsilon;
     }
 }
