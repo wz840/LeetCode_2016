5e4deadb561f6970c0cee1735a72dd115ad4a94e
wz840 <wz840@nyu.edu>
2016-07-27 23:42:59 +0800

    [287. Find the Duplicate Number][Accepted]committed by LeetCode Extension

--- 287-Find-the-Duplicate-Number/solution.java
+++ 287-Find-the-Duplicate-Number/solution.java
@@ -1,11 +1,6 @@
 public class Solution {
     public int findDuplicate(int[] nums) {
-        /*List<Integer> ls = new ArrayList<Integer>();
-        for (int i=0; i<nums.length; i++) {
-            ls.add(nums[i]);
-        }
-        Set<Integer> hs = new HashSet<Integer>(ls);
-        int size = hs.size();*/
+        
         int dup = 0;
         int[] o = new int[nums.length-1];
         for (int i=0; i<nums.length; i++){
