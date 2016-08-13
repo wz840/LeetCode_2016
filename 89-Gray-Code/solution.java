2cdaba32f1fbd2efd15d08d706eafe66791d7224
wz840 <wz840@nyu.edu>
2016-08-13 16:47:42 +0800

    [89. Gray Code][Accepted]committed by LeetCode Extension

--- 89-Gray-Code/solution.java
+++ 89-Gray-Code/solution.java
@@ -1,11 +1,11 @@
 public class Solution {
     public List<Integer> grayCode(int n) {
         List<Integer> result = new ArrayList<Integer>();
-        result.add(0);
-        int base = 1;
-        int totalSize = 1<<n;
+        result.add(0);                      // Always starts with 0
+        int base = 1;                       // Base with which does XOR
+        int totalSize = 1<<n;               // Total size of the result
         while (result.size() < totalSize){
-            List<Integer> temp = new ArrayList<Integer>(result);
+            List<Integer> temp = new ArrayList<Integer>(result); // Resize the temp list
             for (int i=temp.size()-1; i>=0; i--) {
                 result.add(temp.get(i) ^ base);
             }
