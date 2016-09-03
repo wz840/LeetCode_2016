ad6750c64ea34ea68b93c7aabf5a7d1a4cd43651
wz840 <wz840@nyu.edu>
2016-09-04 00:06:04 +0800

    [153. Find Minimum in Rotated Sorted Array][Accepted]committed by LeetCode Extension

--- 153-Find-Minimum-in-Rotated-Sorted-Array/solution.java
+++ 153-Find-Minimum-in-Rotated-Sorted-Array/solution.java
@@ -1,29 +1,39 @@
 public class Solution {
     public int findMin(int[] nums) {
-        /*if (nums.length==1) return nums[0];
+        
+        if (nums.length==1) return nums[0];
         int diff = 0;
-        int index = 0;
+        int index1 = 0;
+        int index2 = 0;
         
         for (int i=0; i<nums.length; i++) {
             diff = nums[i] - nums[nums.length-1-i];
             if (diff<=0) {
-                index = nums.length-i-1;
+                index1 = i;
+                index2 = nums.length-i-1;
                 break;
             }
         }
-        if (nums.length%2!=0 && index==nums.length/2) {
-            if (nums[index]<=nums[index+1] && nums[index]<=nums[index-1]) return nums[index];
-            else return nums[index+1];
+        if (nums.length%2!=0 && index1==nums.length/2) {
+            if (nums[index1]<=nums[index1+1] && nums[index1]<=nums[index1-1]) return nums[index1];
+            else {
+                int index = (nums[index1]<nums[(index2+1)%nums.length])?index1:((index2+1)%nums.length);
+                return nums[index];
+            }
         } else {
-            return nums[(index+1)%nums.length];
-        }*/
-        int min = nums[0];
+            int index = (nums[index1]<nums[(index2+1)%nums.length])?index1:((index2+1)%nums.length);
+            return nums[index];
+        }
+
+        
+        
+        /*int min = nums[0];
         int temp = 0;
         for (int i=0; i<nums.length/2; i++) {
             temp = (nums[i]<=nums[nums.length-1-i])?nums[i]:nums[nums.length-1-i];
             min = (min<=temp)?min:temp;
         }
         min = min<=nums[nums.length/2]?min:nums[nums.length/2];
-        return min;
+        return min;*/
     }
 }
